55e6ddbec9368c748961c280ea1db7963f8ac6a0
Jocelyn <jocelyn725paek@gmail.com>
2023-06-12 08:24:27 -0700

    Initial Commit

---
+++ Github practice/Github_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_practiceApp.swift
+//  Github practice
+//
+//  Created by Jocelyn Paek on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
